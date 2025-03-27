6415f561a258e94a1eb30e074834493959c6f89e
Kevin Tran <khoinguyentph@gmail.com>
2025-03-27 17:29:54 +1030

    delete unused files

--- function-1-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "person.h"
-
-PersonList shallowCopyPersonList(PersonList pl) {
-    PersonList newList;
-    newList.numPeople = pl.numPeople;
-    newList.people = pl.people;
-    return newList;
-}
