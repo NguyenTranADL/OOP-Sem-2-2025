2f7e8c096e8bea8c1d025a22913d641802ba7130
NguyenTranADL <khoinguyentph@gmail.com>
2025-03-28 14:30:01 +1030

    Update function-1-4.cpp

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "Person.h"
 
+PersonList createPersonList(int n) {
+    PersonList list;
+    list.people = new Person[n];
+    list.numPeople = n;
+    for (int i = 0; i < n; i++) {
+        list.people[i].name = "Jane Doe";
+        list.people[i].age = 1;
+    }
+    return list;
+}
+
 PersonList shallowCopyPersonList(PersonList pl) {
     PersonList newList;
     newList.numPeople = pl.numPeople;
