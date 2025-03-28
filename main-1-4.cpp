72d7b6dce22b8e8993bc614a47f5366d4f425e0d
NguyenTranADL <khoinguyentph@gmail.com>
2025-03-28 14:32:54 +1030

    Update main-1-4.cpp

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -3,13 +3,11 @@
 
 extern PersonList createPersonList(int n);
 extern PersonList shallowCopyPersonList(PersonList pl);
-extern PersonList deepCopyPersonList(PersonList pl);
 
 int main(){
     int n = 5;
     PersonList list = createPersonList(n);
     PersonList shallowList = shallowCopyPersonList(list);
-    PersonList deepList = deepCopyPersonList(list);
 
     for (int i = 0; i < n; i++) {
         std::cout << "Person " << i << ": " << list.people[i].name << ", " << list.people[i].age << std::endl;
@@ -18,12 +16,7 @@ int main(){
     for (int i = 0; i < n; i++) {
         std::cout << "Shallow Person " << i << ": " << shallowList.people[i].name << ", " << shallowList.people[i].age << std::endl;
     }
-    std::cout << std::endl;
-    for (int i = 0; i < n; i++) {
-        std::cout << "Deep Person " << i << ": " << deepList.people[i].name << ", " << deepList.people[i].age << std::endl;
-    }
     delete[] list.people;
-    delete[] shallowList.people;
-    delete[] deepList.people;
+    delete[] shallowList.peoplepeople;
     return 0;
 }
