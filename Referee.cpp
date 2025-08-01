06c9983eb91f5e3267fb37b4a4668b0f228d8161
NguyenTranADL <khoinguyentph@gmail.com>
2025-08-01 17:24:18 +0930

    Prac1 Sol

--- Referee.cpp
+++ Referee.cpp
@@ -7,32 +7,15 @@ Referee::Referee() {}
 Player* Referee::refGame(Player* player1, Player* player2) {
   char move1 = player1->makeMove();
   char move2 = player2->makeMove();
-  // Player 1 choose Rock
-  if (move1 == 'R') {
-    if (move2 == 'S') {
-      return player1;
-    } else if (move2 = 'P') {
-      return player2;
-    } else
-      return nullptr;
-  }
-  // Player 1 choose Scissors
-  if (move1 = 'S') {
-    if (move2 = 'P') {
-      return player1;
-    } else if (move2 = 'R') {
-      return player2;
-    } else
-      return nullptr;
-  }
-  // Player 1 chooose Paper
-  if (move1 == 'P') {
-    if (move2 == 'R') {
-      return player1;
-    } else if (move2 == 'S') {
-      return player2;
-    } else
-      return nullptr;
-  }
-  return nullptr;
+ 
+    if (move1 == move2) {
+        return nullptr; // Tie
+    }
+    if ((move1 == 'R' && move2 == 'S') || 
+        (move1 == 'S' && move2 == 'P') || 
+        (move1 == 'P' && move2 == 'R')) {
+        return player1; // Player 1 wins
+    } else {
+        return player2; // Player 2 wins
+    }
 }
