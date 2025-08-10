e314d19af24af13e8f1c14838ac5b18fc0602123
NguyenTranADL <khoinguyentph@gmail.com>
2025-08-10 19:56:16 +0930

    Prac 2 S

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <string>
-#include "Referee.h"
 #include "Human.h"
 #include "Computer.h"
+#include "Referee.h"
 
 int main() {
-    Referee ref;
-    Human *humanPlayer = new Human("Player One");
-    Computer *computerPlayer = new Computer("Comp");
-    Player* winner = ref.refGame(humanPlayer, computerPlayer);
+    Human *h = new Human("Alice");
+    Human *h2 = new Human("Bob");
+    Computer *c = new Computer("Computer");
 
-    if (winner != nullptr) {
-        std::cout << "The winner is: " << winner->getName() << std::endl;
+    Referee referee;
+
+    Player *winner1 = referee.refGame(h, h2);
+    if (winner1) {
+        std::cout << "The winner is: " << winner1->getName() << "!" << std::endl;
     } else {
         std::cout << "It's a draw." << std::endl;
     }
-    delete humanPlayer;
-    delete computerPlayer;
+
+    Player *winner2 = referee.refGame(h, c);
+    if (winner2) {
+        std::cout << "The winner is: " << winner2->getName() << "!" << std::endl;
+    } else {
+        std::cout << "It's a draw." << std::endl;
+    }
+
+    delete h;
+    delete h2;
+    delete c;
+
     return 0;
 }
