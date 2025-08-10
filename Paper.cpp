eaa42355284d8ad5e224f57340c6df8437e116a4
NguyenTranADL <khoinguyentph@gmail.com>
2025-08-10 19:30:40 +0930

    Prac 2 sol

--- Paper.cpp
+++ Paper.cpp
@@ -1,7 +1,8 @@
 #include "Paper.h"
-#include "Rock.h"
+#include "Rock.h"  
+#include "Robot.h" 
 
 bool Paper::beats(const Move& other) const {
     const std::string otherName = other.getName();
-    return otherName == "Rock";
+    return otherName == "Rock" || otherName == "Robot";
 }
