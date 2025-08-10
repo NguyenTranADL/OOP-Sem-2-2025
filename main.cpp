32b3b1f7b8cd6333980290cfa528c9ce2ef719dd
NguyenTranADL <khoinguyentph@gmail.com>
2025-08-10 22:26:54 +0930

    Prac2 Solu

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ int main() {
     Human human("Player1");
     Computer computer("Computer");
 
-    Player* winner = ref.refGame(&human, &computer);
-    if (winner != nullptr) {
+     if (Player* winner = ref.refGame(&human, &computer)) {
         std::cout << "Winner is: " << winner->getName() << std::endl;
     } else {
         std::cout << "It's a draw." << std::endl;
